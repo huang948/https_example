ddedf190c2c4b49ad7912415a0446bed87cf50e3
Jordan Huang <jordanhuang99@gmail.com>
2018-05-14 19:06:03 +0000

    added a callback function

---
+++ getHTML.js
@@ -0,0 +1,34 @@
+var https = require('https');
+
+
+function getHTML (options, callback) {
+
+var body = '';
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      body += data;
+    });
+
+    response.on('end', function () {
+      callback(body);
+    });
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+
+getHTML(requestOptions, printHTML);
